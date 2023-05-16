90bbfeac43c253a026f63a9c969508943c8db853
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-05-16 21:08:18 +0200

    database funcionality finish

--- frontend/main.js
+++ frontend/main.js
@@ -1,3 +1,29 @@
-const data = fetch("http://127.0.0.1:5000/video/1")
-  .then((res) => res.json())
-  .then((data) => console.log(data));
+const putDataBtn = document.querySelector(".put-data-btn");
+const getDataBtn = document.querySelector(".get-data-btn");
+
+putDataBtn.addEventListener("click", () => {
+  const dataToPut = {
+    id: 5,
+    name: "johnyy",
+    views: 5,
+    likes: 5,
+  };
+
+  fetch(`http://127.0.0.1:5000/video/${Math.floor(Math.random() * 1000)}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(dataToPut),
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
+
+getDataBtn.addEventListener("click", () => {
+  fetch("http://127.0.0.1:5000/videos", {
+    method: "GET",
+  })
+    .then((res) => res.json())
+    .then((data) => console.log(data));
+});
