ec22568253e56054f3704ab8eca4b44470171e86
Pawel  Kurek <pawel.kurek975@gmail.com>
2023-05-16 20:24:33 +0200

    fixed cors problem

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_restful import Api, Resource, reqparse, fields, marshal_with, abort
 from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS
 
 app = Flask(__name__)
 api = Api(app)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.db"
 db = SQLAlchemy(app)
+CORS(app)
 
 
 class VideoModel(db.Model):
